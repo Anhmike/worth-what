a3bfbaf41bce8cef2e2052777a147836960dc3b9
blooies <joy28@msn.com>
2016-08-13 15:35:53 -0400

    added loading and parsing of category1

--- training/generate_input.js
+++ training/generate_input.js
@@ -1,3 +1,15 @@
+require('dotenv').config();
+var Clarifai = require('./clarifai-2.0.2/index');
+//model id: b778c20f3d494402b43e79dde1a2a53b
+
+Clarifai.initialize({
+    'clientId': process.env.CLIENT_ID,
+    'clientSecret': process.env.CLIENT_SECRET,
+    'apiEndpoint': 'https://api2-prod.clarifai.com'
+});
+
+fs = require('fs');
+
 function generateInput(imageUrl, id, value) {
     var concepts = [
         'superRich',
@@ -32,5 +44,18 @@ function generateInput(imageUrl, id, value) {
 
     Clarifai.addInputs([
         input
-    ])
+    ]).then(
+        function(response) {
+            console.log(response);
+        },
+        function(error) {
+            console.error(error);
+        }
+    )
+}
+
+var file = fs.readFileSync('Category1', 'utf8');
+var lines = file.split('\n');
+for (var i=0; i<lines.length; i++) {
+    generateInput(lines[i], 'superRich', true);
 }
