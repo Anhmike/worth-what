7d74d74b6d31ca33a2bf72d5c447b70db56c2f30
Johnathan Weisner <joweisner@gmail.com>
2016-08-13 18:59:11 -0400

    So far

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -44,7 +44,24 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	console.log('Hey');
+	
+	document.addEventListener('DOMContentLoaded', function () {
+	    var takePhotoBtn = document.getElementById('take-photo');
+	    var token = document.getElementById('token');
+	    console.log('');
+
+	    Clarifai.initialize({ 'apiEndpoint': 'https://api2-prod.clarifai.com' });
+	    Clarifai.setToken(token.innerText);
+
+	    function uploadPhoto(file) {
+	        var reader = new FileReader();
+
+	        reader.addEventListener('load', function () {});
+	        reader.readAsDataURL(file);
+	        console.log(file);
+	    }
+	    takePhotoBtn.addEventListener('change', uploadPhoto);
+	}, false);
 
 /***/ }
 /******/ ]);
